e48827332ec950335ed53d46ee95d2a76069693c
kaimarshall <kaimarshall@10.242.0.179>
2019-09-09 11:16:04 -0400

    ready

---
+++ Kai Marshall Programming 1 First project/src/KaiMarshallHelloThere.java
@@ -0,0 +1,23 @@
+
+public class KaiMarshallHelloThere {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Project Name: Hello there
+		// Name: Kai A. Marshall
+		
+		System.out.println("My name is Kai Marshall");
+		System.out.println("I am a Computer Science major");
+		System.out.println("I play football");
+		System.out.println("I play offensive guard");
+		System.out.println("I am also a starting freshman");
+		System.out.println("My favorite color is red");
+		System.out.println("I love watching comedy shows\nlike Rick and Morty or Family Guy");
+		System.out.println("I am 18 years old\nI was born November 12, 2000");
+		System.out.println("My favorite Holiday is Christmas");
+		
+		
+	}
+
+}
