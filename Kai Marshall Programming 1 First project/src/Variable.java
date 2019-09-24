23f0e8d88d099a626442c6f36bb88b757bf2c9e0
kaimarshall <kaimarshall@10.242.0.179>
2019-09-23 20:41:47 -0400

    Ready to grade

--- Kai Marshall Programming 1 First project/src/Variable.java
+++ Kai Marshall Programming 1 First project/src/Variable.java
@@ -12,29 +12,111 @@
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		int numberone;
+		int numbertwo;
+		int numberthree;
 
-		// Declare Variables
-		int age; 
-		int favnumber;
-		String name= "Kai";
+		
+		// Start keyboard scanner
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("What is your first number ?");
+		numberone = keyboard.nextInt();
+		System.out.println("what is your second number ?");
+		numbertwo = keyboard.nextInt();
+		System.out.println("What is your third number ?");
+		numberthree = keyboard.nextInt()
+;		 
+		//Declare variables 
+		int choice;
+		
 
-		// Start keyboard scanner 
-		Scanner keyboard = new Scanner (System.in);
+		System.out.println("Main Menu");
+		System.out.println("========/n");
+		System.out.println("1.Add");
+		System.out.println("2.Subtract");
+		System.out.println("3.Multiply");
+		System.out.println("Divide");
+	    System.out.println("Exit");
+	    
+	    
+			choice = keyboard.nextInt();
+			  
+			int operation = 0;
+			
 
-		// Collect input from user
-		System.out.print("What is your name? ");
-		name = keyboard.next();
+			switch (choice)
+			{
+			case 1:
+				operation= numberone + numbertwo;
+			     System.out.println("Sum of the integers = " + operation);				  
+			     break;
+			      
+			case 2:
+				operation= numberone - numbertwo;
+			     System.out.println("Substraction of the integers = " + operation);				  
+			     break;
+			case 3:
+				operation = numberone * numbertwo;
+			     System.out.println("Multiplication of the integers = " + operation);	
+			    break;
+			    
+			case 4:
+				operation = numberone / numbertwo;
+			     System.out.println("Divition of the integers = " + operation);
+			    break;
+			    
+			case 5:
+				System.out.println("Thanks!");
+			    break;
+			    
+			default:
+				System.out.println(" You made an invalid choice");
+			
+					}
+			
+			
+			if(operation > numberthree)
+			    System.out.println(  operation + " is greater than " + numberthree);
+			
+			else
+				if (operation < numberthree)
+					System.out.println(  operation + " is less than " + numberthree);
+				else
+					System.out.println(  operation + " is equal to " + numberthree);
+}
 
-		System.out.print("What is your age? ");
-		age = keyboard.nextInt(); 
+}
+	    	
+	    
+	    	
+	      
+	    	
+	    	
+	    	 
+	    	 
+	    	 
+	     
+	    	 
+		   
+		   
+	   
+		
+		
+		
+		
+		
+		
+		
+		
+	     
+	
 
-		System.out.print("What is your favorite number? ");
-		favnumber = keyboard.nextInt(); 
+	
+		
+	
 
-		// Output User Information
-		System.out.print("\n" + name + " is " + age + " years," + favnumber + " is his favorite number " );
-		}
-
-	}
+	
+		
 
 
